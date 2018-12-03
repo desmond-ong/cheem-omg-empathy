66cc38896b2eb10d42a51e08dd9717aa17803ee4
Xuan <tanqazx@gmail.com>
2018-12-03 17:56:41 +0800

    Enable saving predictions, compare against original annotations.

--- src/combined/fusion.py
+++ src/combined/fusion.py
@@ -5,8 +5,10 @@ from __future__ import print_function
 from __future__ import absolute_import
 
 import os
-
+import joblib
+import pandas as pd
 import numpy as np
+
 from sklearn import svm
 from sklearn.model_selection import ParameterGrid
 from sklearn.model_selection import cross_val_score
@@ -37,14 +39,12 @@ def load_data(train_dir, test_dir, in_dirs, in_names=None):
     print("Done.")
     return train_data, test_data, all_data
 
-def main(train_data, test_data, args):
-    # Concatenate sequences into train and test matrices
+def train(train_data, test_data):
+    # Concatenate training sequences into matrix
     X_train, y_train = zip(*train_data)
     X_train, y_train = np.concatenate(X_train), np.concatenate(y_train)
-    X_test, y_test = zip(*test_data)
-    X_test, y_test = np.concatenate(X_test), np.concatenate(y_test)
-    y_train, y_test = y_train.flatten(), y_test.flatten()
-    
+    y_train = y_train.flatten()
+
     # Set up hyper-parameters for support vector regression
     params = {
         'gamma': ['auto'],
@@ -54,6 +54,7 @@ def main(train_data, test_data, args):
     params = list(ParameterGrid(params))
 
     # Cross validate across hyper-parameters
+    print('---')
     best_ccc = -1
     for p in params:
         print("Using parameters:", p)
@@ -64,20 +65,75 @@ def main(train_data, test_data, args):
                         cache_size=1000)
         model.fit(X_train, y_train)
 
-        # Predict and evaluate on test set
-        print("Evaluating...")
-        y_pred = model.predict(X_test)
-        ccc = eval_ccc(y_test, y_pred)
-        print('CCC:', ccc)
+        # Evaluate on test set
+        ccc, predictions = evaluate(model, test_data)
 
         # Save best parameters and model
         if ccc > best_ccc:
             best_ccc = ccc
             best_params = p 
             best_model = model
+            best_pred = predictions
 
-    print('Best CCC:', best_ccc)
+    # Print best parameters
+    print('---')
+    print('Best CCC: {:0.3f}'.format(best_ccc))
     print('Best parameters:', best_params)
+
+    return best_ccc, best_params, best_model, best_pred
+
+def evaluate(model, test_data):
+    ccc = 0
+    predictions = []
+    # Predict and evaluate on each test sequence
+    print("Evaluating...")
+    for i, (X_test, y_test) in enumerate(test_data):
+        # Get original valence annotations
+        y_test = test_data.val_orig[i].flatten()
+        y_pred = model.predict(X_test)
+        # Repeat and pad predictions to match original data length
+        y_pred = np.repeat(y_pred, test_data.time_ratio)[:len(y_test)]
+        l_diff = len(y_test) - len(y_pred)
+        if l_diff > 0:
+            y_pred = np.concatenate([y_pred, y_pred[-l_diff:]])
+        ccc += eval_ccc(y_test, y_pred)
+        predictions.append(y_pred)
+    ccc /= len(test_data)
+    print('CCC: {:0.3f}'.format(ccc))
+    return ccc, predictions
+
+def save_predictions(pred, dataset, path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    for p, subj, story in zip(pred, dataset.subjects, dataset.stories):
+        df = pd.DataFrame(p, columns=['valence'])
+        fname = "Subject_{}_Story_{}.csv".format(subj, story)
+        df.to_csv(os.path.join(path, fname), index=False)
+        
+def main(train_data, test_data, args):
+    if args.test is None:
+        # Fit new model if test model is not provided
+        ccc, params, model, pred = train(train_data, test_data)
+        # Save best model
+        if not os.path.exists(args.model_dir):
+            os.makedirs(args.model_dir)
+        joblib.dump(model, os.path.join(args.model_dir, "best.save"))
+        # Save predictions of best model
+        pred_dir = os.path.join(args.pred_dir, "test_out")
+        save_predictions(pred, test_data, pred_dir)
+        return ccc
+    else:
+        # Load and test model on training and test set
+        model = joblib.load(args.test)
+        print("-Training-")
+        ccc, pred = evaluate(model, train_data)
+        pred_dir = os.path.join(args.pred_dir, "train_out")
+        save_predictions(pred, train_data, pred_dir)
+        print("-Testing-")
+        ccc, pred = evaluate(model, test_data)
+        pred_dir = os.path.join(args.pred_dir, "test_out")
+        save_predictions(pred, test_data, pred_dir)
+        return ccc
     
 if __name__ == "__main__":
     import argparse
@@ -88,11 +144,13 @@ if __name__ == "__main__":
                         help='names for input features')
     parser.add_argument('--normalize', action='store_true', default=False,
                         help='whether to normalize inputs (default: True)')
+    parser.add_argument('--test', type=str, default=None,
+                        help='path to model to test (default: None)')
     parser.add_argument('--train_dir', type=str, default="./data/Training",
                         help='path to train data (default: ./data/Training)')
     parser.add_argument('--test_dir', type=str, default="./data/Validation",
                         help='path to test data (default: ./data/Validation)')
-    parser.add_argument('--model_dir', type=str, default="./models",
+    parser.add_argument('--model_dir', type=str, default="./fusion_models",
                         help='path to save models')
     parser.add_argument('--pred_dir', type=str, default="./predictions",
                         help='path to save predictions')
